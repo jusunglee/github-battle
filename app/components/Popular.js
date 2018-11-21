e22946084fcfc2120e39e6e2168c7ea96d96c209
Jusung Lee <juice@Jusungs-MacBook-Pro.local>
2018-11-21 17:07:23 -0600

    Separation of SelectedLanguage to stateless function instead of entire React Component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,5 +1,25 @@
 React = require('react')
+var PropTypes = require('prop-types')
 
+function SelectLanguage(props) {
+    var languages = ["All", "Python", "C++", "JavaScript"];
+
+    return(
+        <ul className="languages">
+            {
+                languages.map(function(language) {
+                    return (<li 
+                    onClick = {props.onSelect.bind(null, language)}
+                    style = {props.selectedLanguage === language ? {color : "#d0021b"}: null} 
+                    key = {language}>{language}</li>)}, this)
+            }
+        </ul>
+    )
+}
+SelectLanguage.propTypes = {
+    selectedLanguage: PropTypes.string.isRequired,
+    onSelect: PropTypes.func.isRequired,
+}
 class Popular extends React.Component {
     constructor(props) {
         super(props);
@@ -18,17 +38,14 @@ class Popular extends React.Component {
     }
 
     render() {
-        var languages = ["All", "Python", "C++", "JavaScript"];
         return (
-            <ul className="languages">
-                {
-                    languages.map(function(language) {
-                        return (<li 
-                        onClick = {this.updateLanguage.bind(null, language)}
-                        style = {this.state.selectedLanguage === language ? {color : "#d0021b"}: null} 
-                        key = {language}>{language}</li>)}, this)
-                }
-            </ul>
+            <div>
+                <SelectLanguage
+                    selectedLanguage={this.state.selectedLanguage}
+                    onSelect = {this.updateLanguage}
+                />
+            </div>
+            
         );
     }
 }
